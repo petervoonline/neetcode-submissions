3779b5465b4800bd4d5cf98c490abd8949af9e80
Peter Vo <60068763+petervoonline@users.noreply.github.com>
2026-06-05 15:07:21 -0400

    Add: number-of-students-unable-to-eat-lunch - submission-6

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        preference = {0: 0, 1: 0}
+        for i in students:
+            preference[i] = preference[i] + 1
+
+        for i in sandwiches:
+            if preference[i] > 0:
+                preference[i] = preference[i] - 1
+            else:
+                return preference[0] + preference[1]
+        return 0
+                
+
