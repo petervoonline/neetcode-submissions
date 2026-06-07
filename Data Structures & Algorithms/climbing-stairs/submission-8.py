a18e51b82151d6f424d3e136bf7813d6b1ebcf45
Peter Vo <60068763+petervoonline@users.noreply.github.com>
2026-06-07 10:58:00 -0400

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def __init__(self):
+        self.memo = {}
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        if n not in self.memo:
+            self.memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.memo[n]
